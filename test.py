64a6f6bcbbfe3af754351c7bd7ddb32236f48f4d
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-08-06 21:22:17 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,7 @@
+groceories = [["apple","mango","orange"],["carrot","potato","onion"]
+              ,["fish","chicken","beaf"]]
+
+print(groceories)
+
+#O/P -[['apple', 'mango', 'orange'], ['carrot', 'potato', 'onion'], ['fish', 'chicken', 'beaf']]
+
